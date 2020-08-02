563afef89f1840577e7e647a4f7751e3a43efa1a
xy3 <a@xsq.pw>
2020-08-03 00:16:16 +0100

    Initial Commit

---
+++ src/OneTUI/Views.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace OneTUI;
+/**
+ * Views helper
+ */
+class Views
+{
+    private $service;
+    private $views_dir = "src/views/";
+    private $components_dir = "src/views/components/";
+    private $sharedData;
+
+    function __construct($_service)
+    {
+        $this->service = $_service;
+        $this->sharedData = ['views' => $this->views_dir, 'components' => $this->components_dir];
+    }
+
+    public function home() {
+        $this->service->render($this->views_dir . "home.phtml", $this->sharedData);
+    }
+
+    public function userPage() {
+        $this->service->render($this->views_dir . "home.phtml", $this->sharedData);
+    }
+
+}
